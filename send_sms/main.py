e1137d7c650e536d2485d1116a3118356143719d
VladCristea00 <97668575+VladCristea00@users.noreply.github.com>
2022-11-09 00:21:30 +0100

    Update main.py

--- send_sms/main.py
+++ send_sms/main.py
@@ -1,11 +1,11 @@
 from twilio.rest import Client 
  
-account_sid = '***********e8c3d7a18acf67c75c5eb3c' 
-auth_token = '************22aa0ca29ce6abcd191644b' 
+account_sid = '*********' 
+auth_token = '************' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create(  
-                              messaging_service_sid='*********07b267def14e3aed7d818c', 
+                              messaging_service_sid='*********', 
                               body='Hello there',      
                               to='(enter phone number)' 
                           ) 
